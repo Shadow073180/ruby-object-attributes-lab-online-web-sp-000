2c58924a3d41848abd8ed55160eb314d8ba090f4
David masterson <david73180@gmail.com>
2018-10-01 19:56:27 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,22 +1,5 @@
 class Person
 
-  def intialize(name)
-    @name = name
-  end
-
-  def name
-    @name
-  end
-
-
-
-
-  def intialize(job)
-    @job = job
-  end
-
-  def job
-    @job
-  end
+  
 
 end
